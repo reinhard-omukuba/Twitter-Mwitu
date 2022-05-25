e77f092dcd013e1810fb7e31a17c755384fef728
reinhard-omukuba <reinhardomukuba@gmail.com>
2022-05-25 11:16:19 +0300

    Firebase CDN

---
+++ Js/register.js
@@ -0,0 +1,10 @@
+document.getElementById("signUp").onclick = function(){
+
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+
+    console.log(email + password)
+
+
+}
