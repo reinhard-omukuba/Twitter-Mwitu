b00dad6545b181e41c07317f5e0fa473a65e7ea3
reinhard-omukuba <reinhardomukuba@gmail.com>
2022-05-30 11:50:28 +0300

    Tweet

--- Js/home.js
+++ Js/home.js
@@ -1,9 +1,58 @@
 //checking authentication state
 firebase.auth().onAuthStateChanged((user)=>{
-
     if(user){
-        //if the user is loggen in, do the below task
-        console.log("user is logged in")
+        //getting the user id
+        const userId = user.uid;
+
+        //getting the timestamp
+        const timeStamp = new Date();
+
+        document.getElementById("tweetBtn").onclick = function(){
+            const tweet = document.getElementById("tweetInput").value;
+
+            //sending the tweet to database onclick of a button 
+            const sendTweet = firebase.firestore().collection("tweets").doc();
+            sendTweet.set({
+                userTweet:tweet,
+                userId:userId,
+                timeStamp:timeStamp,
+                docId: sendTweet.id 
+
+            }).then(()=>{
+                window.location.reload();
+            })
+        }
+
+        //onchange of the tweet input
+        document.getElementById("tweetInput").onkeyup = function(){
+
+            const tweet = document.getElementById("tweetInput").value;
+            
+            if(tweet == ""){
+                document.getElementById("disabledBtn").style.display = "block";
+                document.getElementById("tweetBtn").style.display = "none";
+
+            }else{
+                document.getElementById("disabledBtn").style.display = "none";
+                document.getElementById("tweetBtn").style.display = "block";
+            }
+            
+        }
+
+        //getting the username
+
+        firebase.firestore().collection("users").doc(userId).get().then((doc) =>{
+            const username =  doc.data().userName;
+
+            document.getElementById("username").innerText = username;
+        })
+
+
+
+
+
+
+
     }else{
         //if user is not logged in, do the below task
         console.log("user is not logged in")
@@ -11,11 +60,11 @@ firebase.auth().onAuthStateChanged((user)=>{
     }
 })
 
-//signing out a user
-document.getElementById("logout").onclick = function(){
 
-    firebase.auth().signOut().then(()=>{
-        alert("User logged out successfully")
-        window.location.href = "index.html";
-    })
-}
+//signing out a user
+// document.getElementById("logout").onclick = function(){
+//     firebase.auth().signOut().then(()=>{
+//         alert("User logged out successfully")
+//         window.location.href = "index.html";
+//     })
+// }
