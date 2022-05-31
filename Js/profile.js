914ee225bca222561414d53c1c2c5ad98de38d76
reinhard-omukuba <reinhardomukuba@gmail.com>
2022-05-31 11:13:18 +0300

    edit account

--- Js/profile.js
+++ Js/profile.js
@@ -12,12 +12,33 @@ firebase.auth().onAuthStateChanged((user)=>{
         firebase.firestore().collection("users").doc(userId).get().then((doc)=>{
 
             const username = doc.data().userName;
+            const bio = doc.data().bio;
 
             document.getElementById("test").innerText = username 
             document.getElementById("theTitle").innerText = username 
-
+            document.getElementById("profName").innerText = username 
+            document.getElementById("profUserNamed").innerText = "@" +username 
+            document.getElementById("bio").innerText = bio;
+            
+            //fill the edit account page
+            document.getElementById("edtName").value = username;
+            document.getElementById("edtBio").value = bio;
 
         })
+
+        //update acount
+        document.getElementById("saveChanges").onclick = function(){
+
+            var edtName = document.getElementById("edtName").value;
+            var edtBio = document.getElementById("edtBio").value;
+
+            firebase.firestore().collection("users").doc(userId).update({
+                userName:edtName,
+                bio:edtBio
+            }).then(()=>{
+                window.location.reload();
+            })
+        }
 
 
 
